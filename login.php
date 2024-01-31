9d222aebd75356d833745c16dccbc55159461843
wh1t3d3ath <97808411+wh1t3d3ath@users.noreply.github.com>
2024-01-31 11:51:09 +0200

    Update and rename login.html to login.php

---
+++ login.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+    <meta charset="utf-8">
+    <title>Login Form Design | CodeLab</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">
+            Login Form
+        </div>
+        <form action="login-process.php" method="post">
+            <div class="field">
+                <input type="text" name="email" required>
+                <label>Email Address</label>
+            </div>
+            <div class="field">
+                <input type="password" name="password" required>
+                <label>Password</label>
+            </div>
+            <div class="content">
+                <div class="checkbox">
+                    <input type="checkbox" id="remember-me">
+                    <label for="remember-me">Remember me</label>
+                </div>
+                <div class="pass-link">
+                    <a href="forgot-password.php">Forgot password?</a>
+                </div>
+            </div>
+            <div class="field">
+                <input type="submit" value="Login">
+            </div>
+            <div class="signup-link">
+                Not a member? <a href="signup.php">Signup now</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
